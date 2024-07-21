b032f282da5fdd85c3be8fe69a48dd0ead63ea0b
Aaron Mathis <aaron.mathis@gmail.com>
2024-07-21 11:11:32 -0500

    Starting Argparsing class

--- Python/pySQLExport/pySQLExport/__main__.py
+++ Python/pySQLExport/pySQLExport/__main__.py
@@ -1,18 +1,29 @@
 import sys
 import getpass
-from pySQLExport.cli import parse_args
+import os
+from pySQLExport.cli import CLI
 from pySQLExport.config import load_config
 from pySQLExport.database import Database
 from pySQLExport.query import Query
 from pySQLExport.export import export_to_csv
+from pySQLExport.utils import get_db_info_from_user
 
 def main():
-    args = parse_args(sys.argv[1:])
-    config = load_config(args.config_file)
+
+    cli = CLI()
+    args = cli.parse_args(sys.argv[1:])
+
     
+    config = None
+    if args.config_file and os.path.isfile(args.config_file):
+        config = load_config(args.config_file)
+    else:
+        print("Config file not found. Please provide the database information.")
+        config = get_db_info_from_user()
+        
     # Prompt for the password
     password = getpass.getpass(prompt='Enter database password: ')
-    
+
     db = Database(config['host'], config['user'], password, config['database'], config['port'])
     
     query = Query(db)
--- Python/pySQLExport/pySQLExport/cli.py
+++ Python/pySQLExport/pySQLExport/cli.py
@@ -1,8 +1,14 @@
 import argparse
 
-def parse_args(args):
-    parser = argparse.ArgumentParser(description="MySQL CLI Tool")
-    parser.add_argument('--config-file', type=str, required=True, help='Path to the database config file')
-    parser.add_argument('--query', type=str, required=True, help='SQL query to execute')
-    parser.add_argument('--output', type=str, help='Output CSV file to save the results')
-    return parser.parse_args(args)
+class CLI:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="pySQLExport CLI Tool")
+        self._add_arguments()
+
+    def _add_arguments(self):
+        self.parser.add_argument('--config-file', type=str, required=False, help='Path to the database config file')
+        self.parser.add_argument('--query', type=str, required=True, help='SQL query to execute')
+        self.parser.add_argument('--output', type=str, help='Output CSV file to save the results')
+
+    def parse_args(self, args):
+        return self.parser.parse_args(args)
