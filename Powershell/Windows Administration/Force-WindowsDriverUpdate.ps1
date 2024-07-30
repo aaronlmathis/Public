6dee34914e79c23d514f31aba28eca35c2be67d4
Aaron Mathis <aaron.mathis@gmail.com>
2024-07-30 16:56:19 -0500

    Update Force-WindowsDriverUpdate.ps1

--- Powershell/Windows Administration/Force-WindowsDriverUpdate.ps1
+++ Powershell/Windows Administration/Force-WindowsDriverUpdate.ps1
@@ -1,12 +1,29 @@
 function Force-WindowsDriverUpdate(){
+    # Set the execution policy temporarily to allow script execution
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine -Force
     try{
         if (!(Get-Module -ListAvailable -Name PSWindowsUpdate)) {
             Write-Host -ForegroundColor Yellow "PSWindowsUpdate module is not installed. Installing now..."
             Install-Module -Name PSWindowsUpdate -Force -SkipPublisherCheck 
         }
-        Get-WindowsUpdate
-        Install-WindowsUpdate -AcceptAll -AutoReboot
+        Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
     }catch {
         Write-Host -ForegroundColor Red "An error occurred: $_"
     }
+    Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope LocalMachine -Force
 }
+
+
+
+# Check if the PSWindowsUpdate is installed
+if (Get-Module -ListAvailable | Where-Object { $_.Name -eq "PSWindowsUpdate" }) {
+} else {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Module -Name PSWindowsUpdate -Force
+    Import-Module PSWindowsUpdate
+}
+
+# Install Windows updates, accepting all updates and ignoring reboots
+
+
+# Restore the original execution policy
