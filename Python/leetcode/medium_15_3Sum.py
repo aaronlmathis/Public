bd68af99c3d2070fa27eb7d25aa3985488c63be3
Aaron Mathis <aaron.mathis@gmail.com>
2024-08-30 17:07:40 -0500

    Update medium_15_3Sum.py

--- Python/leetcode/medium_15_3Sum.py
+++ Python/leetcode/medium_15_3Sum.py
@@ -1,3 +1,34 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # Skip duplicates
+
+            left = i+1
+            right = len(nums) - 1
+            sum = nums[i] + nums[left] + nums[right]
+            if sum == 0:
+                res.append([nums[i], nums[left], nums[right]])
+                # Move left and right pointers, and skip duplicates
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1                
+                
+                left += 1
+                right -= 1
+                
+            elif sum < 0:
+                left+=1
+            else:
+                right-=1
         
+        return res
+
+
+
+sol = Solution()
+nums = [-1,0,1,2,-1,-4]       
+print(sol.threeSum(nums))
