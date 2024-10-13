15abd45af469aeebf1592a7653c966c47ba37ee8
Aaron Mathis <aaron.mathis@gmail.com>
2024-10-13 10:30:36 -0500

    Update 632_hard_smallest_range_covering_elements_from_k_lists.py

--- Python/leetcode/632_hard_smallest_range_covering_elements_from_k_lists.py
+++ Python/leetcode/632_hard_smallest_range_covering_elements_from_k_lists.py
@@ -1,12 +1,8 @@
 """
 You have k lists of sorted integers in non-decreasing order. Find the smallest range that includes at least one number from each of the k lists.
-
 We define the range [a, b] is smaller than range [c, d] if b - a < d - c or a < c if b - a == d - c.
 
- 
-
 Example 1:
-
 Input: nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
 Output: [20,24]
 Explanation: 
@@ -19,12 +15,33 @@ Input: nums = [[1,2,3],[1,2,3],[1,2,3]]
 Output: [1,1]
 """
 from typing import List
+import heapq
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        ans = []
+        min_heap = []
+        max_value = float('-inf')
 
+        for i in range(len(nums)):
+            heapq.heappush(min_heap, (nums[i][0], i, 0)) # Push (number, list index, element index) into heap
+            max_value = max(max_value, nums[i][0]) # Keep track of the maximum value.
 
-        return ans
+        result = [float('-inf'), float('inf')]   # initialize the answer variable [x, x]
+
+        while min_heap: # while there is still something in min_heap
+            min_value, lst_idx, ele_idx = heapq.heappop(min_heap) #pop the top value off the min heap
+
+            if max_value - min_value < result[1] - result[0]:
+                result = [min_value, max_value]
+
+            if ele_idx + 1 == len(nums[lst_idx]): # Break the loop if end of list
+                break
+            
+            next_value = nums[lst_idx][ele_idx+1]
+            heapq.heappush(min_heap, (next_value, lst_idx, ele_idx+1))
+            max_value = max(max_value, next_value)
+
+        return result
+
 sol = Solution()
 nums = [[4,10,15,24,26],[0,9,12,20],[5,18,22,30]]
 print(sol.smallestRange(nums))
