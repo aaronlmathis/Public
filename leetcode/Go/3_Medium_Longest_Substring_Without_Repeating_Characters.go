16f917938d33a4c08fd6dc8c3ab0a9629293344d
Aaron Mathis <aaron.mathis@gmail.com>
2024-11-19 13:33:17 -0600

    leetcode 3

---
+++ leetcode/Go/3_Medium_Longest_Substring_Without_Repeating_Characters.go
@@ -0,0 +1,64 @@
+/*
+Given a string s, find the length of the longest
+substring
+
+	without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+package main
+
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	var n int = len(s)
+
+	// Quick exit if s is empty.
+	if n == 0 {
+		return 0
+	}
+	// Left pointer, start of window and maxLength of string
+	var start, maxLength int
+
+	// map byte to empty struct for frequency checks
+	seen := make(map[byte]int)
+
+	// Iterate through s, byte by byte
+	for end := 0; end < n; end++ {
+		// for as long as the next character is not unique, shrink window and remove from seen
+		if lastIndex, exists := seen[s[end]]; exists && lastIndex >= start {
+			start = lastIndex + 1
+		}
+
+		// map current byte
+		seen[s[end]] = end
+
+		// if current window length is greater than maxlength, set maxlength to it
+		if end-start+1 > maxLength {
+			maxLength = end - start + 1
+		}
+
+	}
+	//return maxLength
+	return maxLength
+}
+
+func main() {
+	var s string = "bbbbbb"
+	fmt.Println(lengthOfLongestSubstring((s)))
+}
