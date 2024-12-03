ae41f2b00aa78ee2fdf6f4dfd666d25f1953a232
Aaron Mathis <aaron.mathis@gmail.com>
2024-12-03 10:50:53 -0600

    .

--- leetcode/Python3/100_Easy_Same_Tree_.py
+++ leetcode/Python3/100_Easy_Same_Tree_.py
@@ -31,8 +31,10 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
         if not p and not q:
             return True
+        
         if not p or not q:
             return False
         
