0dc9590acb7b1537128c1b07732505f1a5a8e6a5
Aaron Mathis <aaron.mathis@gmail.com>
2025-01-11 08:59:55 -0600

    leetcode 1400

--- leetcode/Go/1400_Medium_Construct_K_Palindrome_Strings.go
+++ leetcode/Go/1400_Medium_Construct_K_Palindrome_Strings.go
@@ -23,10 +23,37 @@ Explanation: The only possible solution is to put each character in a separate s
 
 package main
 
-func canConstruct(s string, k int) bool {
+import (
+	"fmt"
+)
 
+func canConstruct(s string, k int) bool {
+	n := len(s)
+	if n < k {
+		return false
+	}
+	if n == k {
+		return true
+	}
+	letterCounts := make([]int, 26)
+	for _, char := range s {
+		letterCounts[char-'a']++
+	}
+	oddCount := 0
+	for _, count := range letterCounts {
+		if count%2 != 0 {
+			oddCount++
+		}
+	}
+	if oddCount > k {
+		return false
+	}
+
+	return true
 }
 
 func main() {
-
+	s := "annabelle"
+	k := 2
+	fmt.Println(canConstruct(s, k))
 }
