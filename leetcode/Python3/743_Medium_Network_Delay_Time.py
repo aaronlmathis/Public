80fef03c17ec01b8277c2d797450683fc59048bb
Aaron Mathis <aaron.mathis@gmail.com>
2025-02-03 16:01:24 -0600

    Update 743_Medium_Network_Delay_Time.py

--- leetcode/Python3/743_Medium_Network_Delay_Time.py
+++ leetcode/Python3/743_Medium_Network_Delay_Time.py
@@ -19,8 +19,33 @@ Output: -1
  
 """
 from typing import List
+import heapq
+from collections import defaultdict
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
         
+        dist = {}
+        min_heap = [(0, k)]
+        while min_heap:
+            time, node = heapq.heappop(min_heap)
+            if node in dist:
+                continue
+            dist[node] = time
+            for neighbor, travel_time in graph[node]:
+                if neighbor not in dist:
+                    heapq.heappush(min_heap, (time + travel_time, neighbor))
+        print(dist)
+        # If not all nodes are reached, return -1.
+        if len(dist) != n:
+            return -1
 
-        
+        return max(dist.values())
+sol = Solution()
+times = [[2,1,1],[2,3,1],[3,4,1]]
+n = 4
+k = 2
+
+print(sol.networkDelayTime(times, n, k))
