92f5cabf91969f70da8974029e87371469471a2c
Aaron Mathis <aaron.mathis@gmail.com>
2025-03-03 16:26:49 -0600

    .

---
+++ leetcode/Python3/240_Medium_Search_a_2D_Matrix_II.py
@@ -0,0 +1,71 @@
+"""
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-109 <= matrix[i][j] <= 109
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-109 <= target <= 109
+"""
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Helper function - Binary Search
+        def bsearch(val) -> bool:
+            left, right = 0, (R * C - 1)    # Left: first index  Right: last index
+            while left <= right:
+                middle = (left + right) // 2
+                if matrix_1D[middle] == val:
+                    return True
+                elif matrix_1D[middle] > val:
+                    right = middle-1
+                else:
+                    left = middle+1
+            return False
+
+        # Target 5 is at matrix[1][1]
+
+        R, C = len(matrix), len(matrix[0])
+        row, col = 0, C-1
+        while 0 <= row < R and 0 <= col < C:
+            curr = matrix[row][col]
+
+            if curr == target:
+                return True
+            elif curr > target:
+                c-=1
+            else:
+                r+=1
+        
+        return False
+        
+
+sol = Solution()
+  
+matrix =[[1,4],[2,5]]
+target=2
+matrix = [[1,4,7,11,15],
+          [2,5,8,12,19],
+          [3,6,9,16,22],
+          [10,13,14,17,24],
+          [18,21,23,26,30]]
+target = 5  
+print(sol.searchMatrix(matrix, target))
