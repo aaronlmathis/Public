3b7302a4ef2a3a27d7d8eaf4b5ae201f8b030234
Aaron Mathis <aaron.mathis@gmail.com>
2025-03-11 16:57:08 -0500

    Update 338_Easy_Counting_Bits.py

--- leetcode/Python3/338_Easy_Counting_Bits.py
+++ leetcode/Python3/338_Easy_Counting_Bits.py
@@ -32,9 +32,68 @@ from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0] * (n+1)
-        return ans
+        for t in range(1, n+1):
+            i = t
+            oc = 0
+            if i >= 65536:
+                i%=65536
+                oc+=1
+            if i >= 32768:
+                i%=32768
+                oc+=1            
+            if i >= 16384:
+                i%=16384
+                oc+=1
+            if i >= 8192:
+                i%=8192
+                oc+=1
+            if i >= 4096:
+                i%=4096
+                oc+=1
+            if i >= 2048:
+                i%=2048
+                oc+=1
+            if i >= 1024:
+                i%=1024
+                oc+=1
+            if i >= 512:
+                i%=512
+                oc+=1
 
-n=2
-sol = Solution()
+            if i >= 256:
+                i%=256
+                oc+=1
+
+            if i >= 128:
+                i%=128
+                oc+=1
+
+            if i >= 64:
+                i %= 64
+                oc += 1
+            if i >= 32:
+                i %= 32
+                oc += 1
+            if i >= 16:
+                i %= 16
+                oc += 1
+            if i >= 8:
+                i %= 8
+                oc+=1
+            if i >= 4:
+                i %= 4
+                oc += 1
+            if i >= 2:
+                i %= 2
+                oc+=1
+            if i == 1:
+                oc+=1
+            ans[t] = oc
+        
+        return ans
+n =85723
+
+
+sol = Solution()  
 print(sol.countBits(n))
 n=5        
